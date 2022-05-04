daa0eb0dd6b0741e10a9df60b6b4e4af1a1aa7a8
BradWright-csi <61475259+BradWright-csi@users.noreply.github.com>
2022-05-04 12:19:29 -0400

    FINAL FILES

--- js/validatequote.js
+++ js/validatequote.js
@@ -4,6 +4,71 @@
  */
 
  "use strict";
+ function descvalidation(id)
+ {
+     
+     var field = id.value;
+     var good = false;
+     if(field=="")
+     {
+         document.getElementById("descmsg").innerHTML="Please enter a description";
+         document.getElementById("descmsg").className="text-danger";
+     }
+     else
+     {
+        document.getElementById("descmsg").innerHTML="";
+        good = true;
+     }
+ 
+     return good;
+     
+ }
+ 
+ function quantvalidate(id)
+ {
+     var field = id.value;
+     var good = false;
+ 
+     if(field=="")
+     {
+         document.getElementById("quantmsg").innerHTML="Please enter a quantity";
+         document.getElementById("quantmsg").className="text-danger";
+     }
+     else
+     {
+         if(field=="e")
+         {
+             document.getElementById("quantmsg").innerHTML=("Please enter a valid number");
+             document.getElementById("quantmsg").className="text-danger";
+         }
+         else
+         {
+             document.getElementById("quantmsg").innerHTML="";
+             good = true;
+         }
+     }
+ 
+     return good;
+ }
+ 
+ function validate(id)
+ {
+     var descID = document.getElementById("desc");
+     var quantID = document.getElementById("quant");
+     if(descID.value=="" || quantID.value=="")
+     {
+         return false;
+     }
+     else if(quantvalidate(quantID) && emailvalidation(descID))
+     {
+         return true;
+     }
+     else
+     {
+         return false;
+     }
+ }
+ 
  function emailvalidation(id)
  {
      
@@ -33,102 +98,37 @@
      return good;
      
  }
- 
- function validatephone(id)
- {
-     var field = id.value;
-     var good = false;
- 
-     if(field=="")
-     {
-         document.getElementById("phonemsg").innerHTML="Please enter your phone number";
-         document.getElementById("phonemsg").className="text-danger";
-     }
-     else
-     {
-         if(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(field) == false)
-         {
-             document.getElementById("phonemsg").innerHTML=("Please enter a valid phone number");
-             document.getElementById("phonemsg").className="text-danger";
-         }
-         else
-         {
-             document.getElementById("phonemsg").innerHTML="";
-             good = true;
-         }
-     }
- 
-     return good;
- }
- 
- function validatecard(id)
- {
-     var field = id.value;
-     var good = false;
- 
-     if(field=="")
-     {
-         document.getElementById("cardmsg").innerHTML="Please enter your card number (16-digits)";
-         document.getElementById("cardmsg").className="text-danger";
-     }
-     else
-     {
-         if(/^[0-9]{16}$/.test(field) == false)
-         {
-             document.getElementById("cardmsg").innerHTML=("Please enter a valid card number (16-digits)");
-             document.getElementById("cardmsg").className="text-danger";
-         }
-         else
-         {
-             document.getElementById("cardmsg").innerHTML="";
-             good = true;
-         }
-     }
- 
-     return good;
- }
- 
- function validate(id)
- {
-     var emaildID = document.getElementById("email");
-     var phoneID = document.getElementById("phone");
-     var cardID = document.getElementById("creditcard");
-     if(emaildID.value=="" || phoneID.value=="" || cardID.value=="" )
-     {
-         return false;
-     }
-     else if(validatephone(phoneID) && emailvalidation(emaildID) && validatecard(cardID))
-     {
-         return true;
-     }
-     else
-     {
-         return false;
-     }
- }
- 
+
  function validateSignup(id)
  {
-     var emaildID = document.getElementById("email");
+     var emailID = document.getElementById("email");
      var usernameID = document.getElementById("name");
      var passwordID = document.getElementById("pwd");
  
      usernameID.innerHTML=("");
      passwordID.innerHTML=("");
+     emailID.innerHTML=("");
  
-     if(emaildID.value=="" || usernameID.value=="" || passwordID.value=="" )
+     if(emailID.value=="" || usernameID.value=="" || passwordID.value=="" )
      {
          if(usernameID.value=="")
          {
-             document.getElementById("namemsg").innerHTML=("Please enter your name");
-         }   
+            document.getElementById("namemsg").innerHTML=("Please enter your name");
+            document.getElementById("namemsg").className="text-danger";
+        }   
          if(passwordID.value=="")
          {
-             document.getElementById("pwdmsg").innerHTML=("Please enter your password");
-         }   
+            document.getElementById("pwdmsg").innerHTML=("Please enter your password");
+            document.getElementById("pwdmsg").className="text-danger";
+        }   
+         if(emailID.value=="")
+         {
+            document.getElementById("emailmsg").innerHTML=("Please enter your email");
+            document.getElementById("emailmsg").className="text-danger";
+        }   
          return false;
      }
-     else if(emailvalidation(emaildID))
+     else if(emailvalidation(emailID))
      {
          return true;
      }
